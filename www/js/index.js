0442b83ccfd64773c474d7ace5a8d00859ced2c3
Yutaka Kakinuma <kakinuma.yutaka@gmail.com>
2017-08-26 01:26:08 +0900

    メイン画面作成

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,2 @@
+// App logic.
 window.myApp = {};
-
-window.myApp.open = function() {
-  var menu = document.getElementById('menu');
-  menu.open();
-};
-
-window.myApp.load = function(page) {
-  var content = document.getElementById('content');
-  var menu = document.getElementById('menu');
-  content.load(page)
-    .then(menu.close.bind(menu));
-};
