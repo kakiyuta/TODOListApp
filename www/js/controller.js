28e6edd4db8326cd2a4edd6ad36b488d9edd2ce0
Yutaka Kakinuma <kakinuma.yutaka@gmail.com>
2017-08-26 23:58:04 +0900

    新規タスク作成画面遷機能追加(遷移先の画面はカラ)

--- www/js/controller.js
+++ www/js/controller.js
@@ -4,6 +4,11 @@ myApp.controller = {
         //Splitterのサイドバー表示
         page.querySelector('[component="button/menu"]').onclick = function() {
             document.querySelector("#mySplitter").left.toggle();
-        }
+        };
+
+        // 新規タスク作成画面表示
+        page.querySelector('[component="button/new-task"]').onclick = function() {
+            document.querySelector("#myNavigator").pushPage('new_task.html');
+        };
     }
 }
