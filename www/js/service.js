335300c36e51cc794787e97ee84a8c1db0cae153
Yutaka Kakinuma <kakinuma.yutaka@gmail.com>
2017-08-29 00:26:39 +0900

    初期表示用タスクテンプレート作成

---
+++ www/js/service.js
@@ -0,0 +1,76 @@
+myApp.service = {
+    // 初期表示用タスク
+    initTasks : [
+        {
+            title: "Install Node.js",
+            category: "Preparation",
+            description: "Description Sentence",
+            highlight: true,
+            urgent: false
+        },
+        {
+            title: "Install Cordova",
+            category: "Preparation",
+            description: "Description Sentence",
+            highlight: true,
+            urgent: false
+        },
+        {
+            title: "Install Libraly Of OnsenUI",
+            category: "Preparation",
+            description: "Description Sentence",
+            highlight: true,
+            urgent: false
+        },
+        {
+            title: "Git",
+            category: "Preparation",
+            description: "Description Sentence",
+            highlight: true,
+            urgent: false
+        },
+        {
+            title: "Stady HTML",
+            category: "Stady",
+            description: "Description Sentence",
+            highlight: false,
+            urgent: false
+        },
+        {
+            title: "Stady Javascript",
+            category: "Stady",
+            description: "Description Sentence",
+            highlight: false,
+            urgent: false
+        },
+        {
+            title: "Create MainWindow",
+            category: "Programing",
+            description: "Description Sentence",
+            highlight: false,
+            urgent: false
+        },
+        {
+            title: "Create SideManu",
+            category: "Programing",
+            description: "Description Sentence",
+            highlight: false,
+            urgent: false
+        },
+        {
+            title: "Create CreationNewTask",
+            category: "Programing",
+            description: "Description Sentence",
+            highlight: false,
+            urgent: false
+        },
+        {
+            title: "Create TaskDetailInfomation",
+            category: "Programing",
+            description: "Description Sentence",
+            highlight: false,
+            urgent: false
+        }
+    ]
+
+}
