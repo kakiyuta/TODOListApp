96da97430cb4c954c98cc7c46c1d18430ca82225
Yutaka Kakinuma <kakinuma.yutaka@gmail.com>
2017-08-30 00:23:02 +0900

    タスクの"Pending" ←→ "Complate"遷移機能を追加

--- www/js/service.js
+++ www/js/service.js
@@ -1,4 +1,71 @@
 myApp.service = {
+    // タスクサービス
+    tasks: {
+        // タスク作成
+        create: function(data) {
+            // HTMLエレメント作成
+            var taskItem = ons.createElement(
+                '<ons-list-item tappable category="' + myApp.service.categories.parseId(data.category) +'">' +
+                    '<label class="left">' +
+                        '<ons-checkbox></ons-checkbox>' +
+                    '</label>' +
+                    '<div class="center">' +
+                        data.title +
+                    '</div>' +
+                    '<div class="right">' +
+                        '<ons-icon style="color: grey;" size="24px" icon="ion-ios-trash-outline, material:md-delete"></ons-icon>' +
+                    '</div>' +
+                '</ons-list-item>'
+            );
+
+            // エレメントにタスクデータを保存
+            taskItem.data = data;
+
+            // チェック時のComplation処理
+            taskItem.data.onCheckboxChange = function(event) {
+                myApp.service.animators.swipe(taskItem, function() {
+                    var listId = (taskItem.parentElement.id === 'pending-list' && event.target.checked) ? '#complated-list' : '#pending-list';
+                    console.log(listId);
+                    document.querySelector(listId).appendChild(taskItem);
+                });
+            };
+            taskItem.addEventListener('change', taskItem.data.onCheckboxChange);
+
+            // タスクをPending画面に追加
+            var pendingList = document.querySelector('#pending-list');
+            pendingList.insertBefore(taskItem, null);
+        },
+    },
+
+    // カテゴリサービス
+    categories: {
+        // カテゴリ新規作成&カテゴリリストに登録
+        create: function(categoryLabel) {
+
+        },
+
+        // カテゴリ名をIDに変換
+        parseId: function(categoryLabel) {
+            return categoryLabel ? categoryLabel.replace(/\s\s+/g, ' ').toLowerCase() : '';
+        }
+    },
+
+    // アニメーションサービス
+    animators: {
+        // Swipe
+        swipe: function(listItem, callback) {
+            var animation = (listItem.parentElement.id === 'pending-list') ? 'animation-swipe-right' : 'animation-swipe-left';
+            listItem.classList.add('hide-children');
+            listItem.classList.add(animation);
+
+            setTimeout(function() {
+                listItem.classList.remove(animation);
+                listItem.classList.remove('hide-children');
+                callback();
+            }, 950);
+        },
+    },
+
     // 初期表示用タスク
     initTasks : [
         {
@@ -31,7 +98,7 @@ myApp.service = {
         },
         {
             title: "Stady HTML",
-            category: "Stady",
+            category: "",
             description: "Description Sentence",
             highlight: false,
             urgent: false
@@ -68,6 +135,13 @@ myApp.service = {
             title: "Create TaskDetailInfomation",
             category: "Programing",
             description: "Description Sentence",
+            highlight: false,
+            urgent: false
+        },
+        {
+            title: "Category Test",
+            category: "foo bar HOGE",
+            description: "",
             highlight: false,
             urgent: false
         }
