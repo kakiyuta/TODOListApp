99f2e351af4d96e0b0ef91b65eb4471a0715052f
Yutaka Kakinuma <kakinuma.yutaka@gmail.com>
2017-09-03 23:02:12 +0900

    Task更新機能追加

--- www/js/controller.js
+++ www/js/controller.js
@@ -17,5 +17,57 @@ myApp.controller = {
         // カテゴリ"All", "No Category"のフィルター機能を実装
         myApp.services.categories.bindOnCheckboxChange(page.querySelector('#default-category-list ons-list-item[category-id=""]'));
         myApp.services.categories.bindOnCheckboxChange(page.querySelector('#default-category-list ons-list-item:not([category-id])'));
+    },
+
+    //タスク詳細ページコントローラー
+    detailsTaskPage: function(page) {
+        // pushPageされたタスク情報を取得
+        var element = page.data.element;
+
+        // タスクの情報を画面に表示
+        page.querySelector('#title-input').value = element.data.title;
+        page.querySelector('#category-input').value = element.data.category;
+        page.querySelector('#description-input').value = element.data.description;
+        page.querySelector('#highlight-input').checked = element.data.highlight;
+        page.querySelector('#urgent-input').checked = element.data.urgent;
+
+        // Saveボタン押下イベント
+        page.querySelector('[component="button/save-task"]').onclick = function() {
+            var newTitle = page.querySelector('#title-input').value;
+
+            if (newTitle) {
+                //タスク更新確認ダイアログ
+                ons.notification.confirm(
+                    {
+                        title: 'Save changes?',
+                        message: 'Previous data will be oberwritten.',
+                        buttonLabels: ['Discard', 'Save']
+                    }
+                ).then(function(buttonIndex) {
+                    // Save選択時の処理
+                    if (buttonIndex === 1) {
+                        myApp.services.tasks.updata(element,
+                            {
+                                title: newTitle,
+                                category: page.querySelector('#category-input').value,
+                                description: page.querySelector('#description-input').value,
+                                highlight: page.querySelector('#highlight-input').checked,
+                                urgent: element.data.urgent
+                            }
+                        );
+
+                        // フィルター設定をALLに変更
+                        document.querySelector('#default-category-list ons-list-item ons-radio').checked = true;
+                        // カテゴリ一覧を更新
+                        document.querySelector('#default-category-list ons-list-item').updateCategoryView();
+                        // ページを戻る
+                        document.querySelector('#myNavigator').popPage();
+                    }
+                });
+            } else {
+                // error
+                ons.notification.alert('You must provide a task title.');
+            }
+        };
     }
 }
--- www/js/service.js
+++ www/js/service.js
@@ -72,6 +72,26 @@ myApp.services = {
                 myApp.services.categories.updateRemove(taskItem.data.category);
             });
         },
+
+        updata: function(taskItem, data) {
+
+            if (data.title !== taskItem.data.title) {
+                // タイトル更新
+                taskItem.querySelector('.center').innerHTML = data.title;
+            }
+
+            if (data.category !== taskItem.data.category) {
+                // カテゴリ更新
+                taskItem.setAttribute('category', myApp.services.categories.parseId(data.category));
+                myApp.services.categories.updateAdd(data.category);
+                myApp.services.categories.updateRemove(taskItem.data.category);
+            }
+
+            // ハイライト設定
+            taskItem.classList[data.highlight ? 'add' : 'remove']('highlight');
+
+            taskItem.data = data;
+        }
     },
 
     // カテゴリサービス
